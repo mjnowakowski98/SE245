6ea1b3c01c319c932502bc88c23faf17fe5e2aeb
Matthew Nowakowski <mjnowakowski@email.neit.edu>
2018-07-24 16:49:58 -0400

    Lab 2 Draft complete

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -6,66 +6,86 @@ using System.Threading.Tasks;
 
 namespace Lab2 {
 	class Program {
-		struct EmployeeInfo {
-			String sEmployeeName;
-			Double numHourlyRate,
+		// Small object to store info per employee
+		public struct EmployeeInfo {
+			public String sEmployeeName;
+			public Double
+				numHourlyRate,
 				numHoursWorked;
 				
-			Double numGrossPay,
+			public Double
+				numGrossPay,
 				numGrossPerYear,
-				numNetPay;
+				numNetPay,
+				numTaxesDeducted;
 		}
 
 		static void Main(string[] args) {
 			// Constants
 			const int kNumWeeksPerYear = 52;
-
-			Int32[] kTaxCutoffs = { // Cutoff income for tax brackets
+			Double[] kTaxCutoffs = { // Cutoff income for tax brackets
 				9525, 38700, 82500, 157500, 200000, 500000
 			};
-
 			Double[] kTaxPercents = { // Percentages for tax brackets
 				.10, .12, .22, .24, .32, .35, .37
 			};
 
-			// Output program name
-			Console.WriteLine("Payroll Calculator");
+			Console.WriteLine("Payroll Calculator"); // Output program name
 
-			List<EmployeeInfo> employees = new List<EmployeeInfo>();
+			List<EmployeeInfo> employees = new List<EmployeeInfo>(); // Hold information on all employees
 
 			// Main application loop
 			while (true) {
+				EmployeeInfo tmp = new EmployeeInfo(); // Builder struct
+
+				Console.Write("\n");
+
 				// Get employee name
-				String tmpName = "";
 				do {
 					Console.Write("Enter employee name (type 'done' to stop): ");
-					tmpName = Console.ReadLine();
-				} while (tmpName == "");
-				if (tmpName == "done") break; // Stop entering names if done
+					tmp.sEmployeeName = Console.ReadLine();
+				} while (tmp.sEmployeeName == "");
+				if (tmp.sEmployeeName == "done") break; // Stop entering names if done
 
-				String inputBuffer = "";
-				Double tmpHourlyRate = 0;
-				do {
+				String inputBuffer = ""; // Buffer to hold input to number
+				do { // Input hourly rate
 					Console.Write("Enter hourly pay rate: ");
 					inputBuffer = Console.ReadLine();
-				} while (!Double.TryParse(inputBuffer, out tmpHourlyRate));
+				} while (!Double.TryParse(inputBuffer, out tmp.numHourlyRate));
 
-				inputBuffer = "";
-				Double tmpHours = 0;
-				do {
+				inputBuffer = ""; // Clear buffer
+				do { // Input hours worked
 					Console.Write("Enter hours worked: ");
 					inputBuffer = Console.ReadLine();
-				} while (!Double.TryParse(inputBuffer, out tmpHours));
+				} while (!Double.TryParse(inputBuffer, out tmp.numHoursWorked));
 
-				
+				// Calculate gross pay per week & estimate per year
+				tmp.numGrossPay = tmp.numHourlyRate * tmp.numHoursWorked;
+				tmp.numGrossPerYear = tmp.numGrossPay * kNumWeeksPerYear;
 
-				employees.Add(new EmployeeInfo(tmpName, tmpHourlyRate, tmpHours));
+				// Set tax percentage
+				ushort taxNdx = 0;
+				while (tmp.numGrossPerYear > kTaxCutoffs[taxNdx]) { taxNdx++; }
+				tmp.numNetPay = tmp.numGrossPay - (tmp.numGrossPay * kTaxPercents[taxNdx]); // Calculate net pay
+				tmp.numTaxesDeducted = tmp.numGrossPay * kTaxPercents[taxNdx]; // Calculate deducted taxes
+
+				employees.Add(tmp); // Copy builder to new ndx of employee list
 			}
 
+			// Output information
+			foreach(EmployeeInfo obj in employees) {
+				Console.WriteLine("\n"); // 2 newlines
+				Console.WriteLine("Payroll information for employee: " + obj.sEmployeeName);
+				Console.WriteLine("Hourly rate:                      " + obj.numHourlyRate);
+				Console.WriteLine("Hours worked:                     " + obj.numHoursWorked);
+				Console.WriteLine("Gross pay:                        " + obj.numGrossPay);
+				Console.WriteLine("Estimated yearly gross pay:       " + obj.numGrossPerYear);
+				Console.WriteLine("Taxes deducted:                   " + obj.numTaxesDeducted);
+				Console.WriteLine("Net pay this week:                " + obj.numNetPay);
+			}
 
-
-			// Emulate dos shell pause
-			Console.WriteLine("Press any key to continue");
+			// Emulate DOS shell pause
+			Console.WriteLine(" Press any key to continue . . . ");
 			Console.ReadKey();
 		}
 	}
