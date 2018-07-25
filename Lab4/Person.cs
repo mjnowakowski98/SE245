b1b0ed069d8f8c133f1a0a7d17b02ad645a75004
001392657 <001392657@S111D3>
2018-07-25 14:39:10 -0400

    Lab4 Started - Needs review

---
+++ Lab4/Person.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4 {
+	class Person {
+		// Example public variables
+		// public String
+		// 	fName,
+		// 	mName,
+		// 	lName,
+		// 	street1,
+		// 	street2,
+		// 	city,
+		// 	state,
+		// 	zip,
+		// 	phone,
+		// 	email;
+
+		private String
+			fName,
+			mName,
+			lName,
+			street1,
+			street2,
+			city,
+			state,
+			zip,
+			phone,
+			email;
+
+		public String FirstName {
+			get { return fName; }
+			set { fName = value; }
+		}
+
+		public String MiddleName {
+			get { return mName; }
+			set { mName = value; }
+		}
+
+		public String LastName {
+			get { return lName; }
+			set { lName = value;  }
+		}
+
+		public String Street1 {
+			get { return street1; }
+			set { street1 = value; }
+		}
+
+		public String Street2 {
+			get { return street2; }
+			set { street2 = value; }
+		}
+
+		public String City {
+			get { return city; }
+			set { city = value; }
+		}
+
+		public String State {
+			get { return state; }
+			set { state = value; }
+		}
+
+		public String Zip {
+			get { return zip; }
+			set { zip = value; }
+		}
+
+		public String Phone {
+			get { return phone; }
+			set { phone = value; }
+		}
+
+		public String Email {
+			get { return email; }
+			set { email = value; }
+		}
+
+		public void Output() {
+			Console.WriteLine(FirstName);
+			Console.WriteLine(MiddleName);
+			Console.WriteLine(LastName);
+			Console.WriteLine(Street1);
+			Console.WriteLine(Street2);
+			Console.WriteLine(City);
+			Console.WriteLine(State);
+			Console.WriteLine(Zip);
+			Console.WriteLine(Phone);
+			Console.WriteLine(Email);
+
+			// Example output using public vars
+			// Console.WriteLine(fName);
+			// Console.WriteLine(mName);
+			// Console.WriteLine(lName);
+			// Console.WriteLine(street1);
+			// Console.WriteLine(street2);
+			// Console.WriteLine(city);
+			// Console.WriteLine(state);
+			// Console.WriteLine(zip);
+			// Console.WriteLine(phone);
+			// Console.WriteLine(email);
+		}
+	}
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4 {
+	// Example structure
+	// public struct Person {
+	// 	public String
+	// 		fName,
+	// 		mName,
+	// 		lName,
+	// 		street1,
+	// 		street2,
+	// 		city,
+	// 		state,
+	// 		zip,
+	// 		phone,
+	// 		email;
+	// }
+
+	class Program {
+		static void Main(string[] args) {
+			Person person = new Person();
+			Console.Write("Enter first name: ");
+			person.FirstName = Console.ReadLine();
+			// person.fName = Console.ReadLine();
+			// person.fName += "-Edited";
+
+			Console.Write("Enter middle name: ");
+			person.MiddleName = Console.ReadLine();
+			// person.mName = Console.ReadLine();
+
+			Console.Write("Enter last name: ");
+			person.LastName = Console.ReadLine();
+			// person.lName = Console.ReadLine();
+
+			Console.Write("Enter street1");
+			person.Street1 = Console.ReadLine();
+			// person.street1 = Console.ReadLine();
+
+			Console.Write("Enter street2");
+			person.Street2 = Console.ReadLine();
+			// person.street2 = Console.ReadLine();
+
+			Console.Write("Enter city: ");
+			person.City = Console.ReadLine();
+			// person.city = Console.ReadLine();
+
+			Console.Write("Enter state: ");
+			person.State = Console.ReadLine();
+			// person.state = Console.ReadLine();
+
+			Console.Write("Enter zip code: ");
+			person.Zip = Console.ReadLine();
+			// person.zip = Console.ReadLine();
+
+			Console.Write("Enter phone: ");
+			person.Phone = Console.ReadLine();
+			// person.phone = Console.ReadLine();
+
+			Console.Write("Enter email: ");
+			person.Email = Console.ReadLine();
+			// person.email = Console.ReadLine();
+			person.Output();
+
+			// Example struct inputput
+			// Person person = new Person();
+			// person.fName = Console.ReadLine();
+			// person.mName = Console.ReadLine();
+			// person.lName = Console.ReadLine();
+			// person.street1 = Console.ReadLine();
+			// person.street2 = Console.ReadLine();
+			// person.city = Console.ReadLine();
+			// person.state = Console.ReadLine();
+			// person.zip = Console.ReadLine();
+			// person.phone = Console.ReadLine();
+			// person.email = Console.ReadLine();
+
+			// Example struct output
+			// Console.WriteLine(person.fName);
+			// Console.WriteLine(person.mName);
+			// Console.WriteLine(person.lName);
+			// Console.WriteLine(person.street1);
+			// Console.WriteLine(person.street2);
+			// Console.WriteLine(person.city);
+			// Console.WriteLine(person.state);
+			// Console.WriteLine(person.zip);
+			// Console.WriteLine(person.phone);
+			// Console.WriteLine(person.email);
+		}
+	}
+}
